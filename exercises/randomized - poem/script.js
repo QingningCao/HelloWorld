693738a305a20c47b035f588a5a4a2de4701aa7b
QingningCao <qc2290@columbia.edu>
2021-12-20 01:11:18 -0500

    Create script.js

---
+++ exercises/randomized - poem/script.js
@@ -0,0 +1,32 @@
+let array_one = [
+    "a house of wood",
+    "a house of brick",
+    "a house of broken dishes",
+    "a house of discarded clothing",
+    "a house of dust"
+  ];
+
+  let array_two = [
+    "among other houses",
+    "among small hills",
+    "among high montains",
+    "by a river",
+    "by an abandoned lake"
+  ];
+
+  function renderPoem(){
+    // 1. grab random element from array one
+    let element_from_array_one = array_one[Math.floor(Math.random()*array_one.length)];
+
+    // 2. grab random element from array two
+    let element_from_array_two = array_two[Math.floor(Math.random()*array_two.length)];
+
+    // 3. create a new paragraph element
+
+    // 4. append the paragraph to the div
+
+
+  }
+
+  let btn = document.querySelector("button");
+  btn.addEventListener("click", renderPoem);
